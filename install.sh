6fc0911e47a0a64db960d6cb878982c5cfbcd040
Devops_Programming_Tools <112689510+Task0209@users.noreply.github.com>
2023-05-26 12:17:43 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install -y curl wget apt-transport-https
+sudo minikube start --driver=docker
+minikube start --driver=none
+sudo apt-get install conntrack
+sudo minikube start --driver=docker
+minikube start --driver=none
+minikube start --vm-driver=none
+minikube start
+minikube status
+kubectl get nodes

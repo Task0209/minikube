6659621d616d40ada3560346ce39d7bc4890e137
Devops_Programming_Tools <112689510+Task0209@users.noreply.github.com>
2023-05-26 22:08:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
+########https://www.linuxtechi.com/how-to-install-minikube-on-ubuntu/###########
+##### Installation Guide for Minikube for UBUNTU Machine #################
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y curl wget apt-transport-https
-sudo minikube start --driver=docker
-minikube start --driver=none
-sudo apt-get install conntrack
-sudo minikube start --driver=docker
-minikube start --driver=none
-minikube start --vm-driver=none
-minikube start
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+ls -l
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+minikube version
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+ls -l
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+ls -l
+kubectl version -o yaml
+minikube start --driver=docker
 minikube status
+kubectl cluster-info
 kubectl get nodes
+kubectl get pod -A
+kubectl get svc -A
